479f593d40e18358f0bec3da1ccbdb16d8779dac
hitruk <hitruk@mail.ru>
2021-12-20 21:09:49 +0300

    Version: 0.1, golden copy

--- parser_code/parser_new.py
+++ parser_code/parser_new.py
@@ -1,9 +1,9 @@
 
-
-# from obj_parser.database import DatabaseParser
+from db_obj.db import DbParser
 from obj_parser.pages import HttpQuery, ElementPageParent, ElementPageChild, ElementPageGrandchildren
 import os
 import re
+from config import config
 
 def query_parent_page(BASE_URL):
     """ Get parent html page """
@@ -30,24 +30,26 @@ def element_parent_page(html):
     
     elements = ElementPageParent(html)
     data_parent = elements.get_parent_element()
-    return data_parent
+    print('Got elements parent page: (title, url)')
+    return data_parent # [(title, url)]
 
 def save_data_parent(data_parent):
     """ Save the data parent page in the database """
 
-    db = DatabaseParser()
-    db.insert_table_troops()
-    print('Data parent page saved in the database table: [(troops.type_name, troops.url)]')
+    db = DbParser()
+    db.insert_table_troops(data_parent)
+    print('Saved data parent page in database table: troops', '\n') 
 
+
+# получаем дочерние страницы
 def get_url_child(BASE_URL, data_parent):
     """ Get url child from list urls child """
 
     # в дальнейшем использовать цикл, чтобы получить каждый url_child из списка
-    # for row in data_parent:
-    #     
-    url_child = BASE_URL + data_parent[0][1]
-    print(url_child)
-    return url_child
+    for row in data_parent:
+        url_child = BASE_URL + row[1]
+        print('Got url child page:', url_child)
+        yield url_child
 
 def count_child(url_child):
     """ Get the number of dynamically loaded pages """
@@ -55,7 +57,9 @@ def count_child(url_child):
     query = HttpQuery(url_child, params=None)
     html = query.get_page_html()
     elements = ElementPageChild(html)
-    count_page = elements.count_page()  # количество динамически подгружаемых страниц
+    count_page = int(elements.count_page())  # количество динамически подгружаемых страниц
+    print('Received all dynamically loaded pages:', count_page)
+    #print(type(count_page))
     return count_page
 
 def query_child_page(url_child, count_page):
@@ -80,92 +84,59 @@ def element_child_page(html_child):
     """ Get the elements a child page """
 
     elements = ElementPageChild(html_child)
-    data_child = elements.get_child_element()
-    print(data_child[0])
-    return data_child[0]
+    data_child = elements.get_child_element() # [(title, url)]
+    print('Got elements child page: [(title, url)]')
+    return data_child
+
+#!
+def get_id_troops(url_child, BASE_URL):
+    """ select id from troops where url_child = ...; """
+    
+    child_url = url_child.replace(BASE_URL, '')
+    conn = DbParser()
+    id_troops = conn.select_id_troops(child_url)
+    print('Got id referencing key table(troops):', id_troops)
+    return id_troops
+
+def mod_data_child(data_child, id_troops):
+    """  """
+    
+    db_data_child = []
+    for row in data_child:
+        new_row = (id_troops, row[0], row[1])
+        db_data_child.append(new_row)
+    print('Generated data to be entered into the table(veterans): [(id_troops, title, url)]')
+    return db_data_child
+
+def save_data_child(db_data_child):
+    """   """
+    
+    conn = DbParser()
+    conn.insert_table_veterans(db_data_child)
+    print('Saved data child page in database table: veterans')
+
 
 def query_grandchild_page(BASE_URL, data_child):
     """ Get grandchild html page and elements """
 
-    url_grandchild = BASE_URL + data_child[1]
-    query = HttpQuery(url_grandchild, params=None)
-    html = query.get_page_html()
-    elements = ElementPageGrandchildren(html)
-    text = elements.get_grandchildren_element()
-    print(text)
-
-# old_code
-def data_parent_page(BASE_URL):
-    """ Get parent page data """
-
-    query = HttpQuery(BASE_URL, params=None)
-    html = query.get_page_html()
-
-    elements = ElementPageParent(html)
-    data_page_parent = elements.get_parent_element()  # [(title, url)]
-    
-    db = DatabaseParser()
-    db.insert_table_troops(data_page_parent)
-    print('function executed: save_data_parent()')
-    return data_page_parent
-
-def extract_data_troops():
-    """ SELECT troops_id, url FROM troops """
-
-    db = DatabaseParser()
-    data_table_troops = db.select_table_troops()  # Данные таблицы troops: troops_id, url
-    print(data_table_troops)
-    for row in data_table_troops:
-        yield row  # [(troops_id, url)]
-
-
-def data_child_page(url, id_troops, BASE_URL):
-    """ Get child page data """
-
-    # страницы подгружаются пролистыванием
-    # количество подгружаемых страниц неизвестно
-    # получаем все подгружаемые страницы
-    n = 1  # номерация начинается с 1
-    one = []  # список, необходимый для сравнения
-    # блок для получения всех страниц, добавляющихся пролистыванием, скрол
-    while True:
-        params = {'page': n}
-        query = HttpQuery(url, params)
-        html = query.get_page_html()
-        if html == None:
-            break
-        elements = ElementPageChild(html)
-        # (id_troops, title, url), для вставки в таблицу veterans
-        data_child = elements.get_child_element(id_troops)
-        if one[:-1] != data_child[:-1]:  # !!!! [:-1]
-            one = data_child
-        else:
-            break
-        n += 1
-    print(len(data_child))  # data_child: (id_troops, title, url)
-
-    db = DatabaseParser()
-    db.insert_table_veterans(data_child)
-
-    data_grandchildren = []
-    m = 1
+    data_grandchild = []
     for row in data_child:
-        # print(row[0])  # id_troops
-        # print(row[1])  # title
-        # print(row[2])  # url
-        url_grandchild = BASE_URL + row[2]
-        print(m, url_grandchild)
-        # --- get grandchild page data ---
+        # print(BASE_URL+row[1]) 
+        url_grandchild = BASE_URL + row[1]
         query = HttpQuery(url_grandchild, params=None)
         html = query.get_page_html()
         elements = ElementPageGrandchildren(html)
         text = elements.get_grandchildren_element()
-        data_grandchildren.append((text, row[2]))  # (text, url), url - grandchild page
-        m += 1
-    print(len(data_grandchildren))
-    db = DatabaseParser()
-    db.insert_table_veterans_one(data_grandchildren)
+        data_grandchild.append((text, row[1]))
+    print('Got data from grandchildren page')    
+    return data_grandchild
+ 
+def save_data_grandchild(data_grandchild):
+    """  """
 
+    conn = DbParser()
+    conn.update_table_veterans(data_grandchild)
+    print('Grandson page data saved in the database, table: veterans', '\n') 
 
 if __name__ == "__main__":
     
@@ -180,24 +151,19 @@ if __name__ == "__main__":
     
     html_parent = open_parent_html()
     data_parent = element_parent_page(html_parent)
-    # save_data_parent(data_parent)
-    url_child = get_url_child(BASE_URL, data_parent)
-    count_child = count_child(url_child)
-    html_child = query_child_page(url_child, count_child)
-    save_html_child(html_child, url_child)
-    element_child_page(html_child)
+    save_data_parent(data_parent)
+    #url_child_one = get_url_child(BASE_URL, data_parent)
+
+    for url_child in get_url_child(BASE_URL, data_parent):
+            
+        count_page = count_child(url_child)
+        html_child = query_child_page(url_child, count_page)
+        save_html_child(html_child, url_child)
+        data_child = element_child_page(html_child) 
     
-
-    #query_grandchild_page(BASE_URL, data_child)
-
-
-    # old_code
-    ''' BASE_URL = 'https://iremember.ru'
-
-    data_parent_page(BASE_URL)
-    for row in extract_data_troops():
-        id_troops = row[0]
-        url = BASE_URL + row[1]
-        print('!')
-        print(url)
-        data_child_page(url, id_troops, BASE_URL)'''
+        id_troops = get_id_troops(url_child, BASE_URL) 
+        db_data_child = mod_data_child(data_child, id_troops)
+        save_data_child(db_data_child) 
+    
+        data_grandchild = query_grandchild_page(BASE_URL, data_child)
+        save_data_grandchild(data_grandchild)
